adb15bf67ab083ad4b9b30ecb7d66cb60d138f7e
Arsen-bjj <78781689+Arsen-bjj@users.noreply.github.com>
2021-03-16 14:34:21 +0600

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n=(int(input('n=')))
+for i in range(n):
+    for j in range(i):
+        print('#',end="")
+    print('')
+for i in range(n,0,-1):
+    for i in range(i):
+     print('#',end="")
+    print('')
+
